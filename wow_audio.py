93427329129154ff986c8171ceb3d3b50dd6445f
Andreas <tece0@hotmail.com>
2021-09-12 14:50:43 +0200

    Added dist/bot to project and mp4 files

--- wow_audio.py
+++ wow_audio.py
@@ -1,21 +1,21 @@
-directory = r'C:\Users\Andreas\PycharmProjects\Discord\owen'
+directory = r'/home/pi/Scripts/Discord/owen'
 
-wow_list = [r'{}\Wow Owen Wilson Sound Effect (download).mp4'.format(directory),
-            r'{}\wow1.mp3'.format(directory),
-            r'{}\wow2.mp3'.format(directory),
-            r'{}\wow3.mp3'.format(directory),
-            r'{}\wow4.mp3'.format(directory),
-            r'{}\wow5.mp3'.format(directory),
-            r'{}\wow6.mp3'.format(directory),
-            r'{}\wow7.mp3'.format(directory),
-            r'{}\wow8.mp3'.format(directory),
-            r'{}\wow9.mp3'.format(directory),
-            r'{}\wow10.mp3'.format(directory),]
+wow_list = [r'{}/Wow Owen Wilson Sound Effect (download).mp4'.format(directory),
+            r'{}/wow1.mp3'.format(directory),
+            r'{}/wow2.mp3'.format(directory),
+            r'{}/wow3.mp3'.format(directory),
+            r'{}/wow4.mp3'.format(directory),
+            r'{}/wow5.mp3'.format(directory),
+            r'{}/wow6.mp3'.format(directory),
+            r'{}/wow7.mp3'.format(directory),
+            r'{}/wow8.mp3'.format(directory),
+            r'{}/wow9.mp3'.format(directory),
+            r'{}/wow10.mp3'.format(directory),]
 
-big_wow = [r'{}\wowwow.mp3'.format(directory)]
+big_wow = [r'{}/wowwow.mp3'.format(directory)]
 
-scubaba = [r'{}\scubaba.mp3'.format(directory)]
+scubaba = [r'{}/scubaba.mp3'.format(directory)]
 
-ole_wedel = [r'{}\ole_wedel.mp3'.format(directory)]
+ole_wedel = [r'{}/ole_wedel.mp3'.format(directory)]
 
-wololo = [r'{}\Wololo Sound Effect.mp3'.format(directory)]
+wololo = [r'{}/Wololo Sound Effect.mp3'.format(directory)]
